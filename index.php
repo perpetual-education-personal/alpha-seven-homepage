2e85ec519d83a47b4db991919e492512808cd6bf
Craig Sanders <126110836+craigsanders22@users.noreply.github.com>
2023-09-24 12:48:24 -0400

    page update! cleaned up design, updated with some grid

--- index.php
+++ index.php
@@ -3,39 +3,38 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Alpha Seven Index!! v3.0</title>
-    <link href="site.css" rel="stylesheet"/>
+    <title>Alpha Seven Index!! V.2</title>
+    <link href="Site.css" rel="stylesheet"/>
   </head>
   <body>
       <h1>Alpha Seven Index</h1>
-      
       <div id="grid-container">
-       <div class="grid-item">
-            <img src="7.png">
-            <a href='ad'>
-              <button>Don't you dare click on Ad's link</button>
-            </a>
-          </div>
           <div class="grid-item">
-            <img src="7.png">
+            <img src="placeholder.png">
             <a href='craig'>
               <button>Craig</button>
             </a>
           </div>
           <div class="grid-item">
-            <img src="7.png">
+            <img src="placeholder.png">
+            <a href='ad'>
+              <button>Don't you dare click on Ad's link</button>
+            </a>
+          </div>
+          <div class="grid-item">
+            <img src="placeholder.png">
             <a href='hunter'>
               <button>Hunter</button>
             </a>
           </div>
           <div class="grid-item">
-            <img src="7.png">
+            <img src="placeholder.png">
             <a href='maxs'>
               <button>Max</button>
             </a>
           </div>
           <div class="grid-item">
-            <img src="7.png">
+            <img src="placeholder.png">
             <a href='paul'>
               <button>Paul</button>
             </a>
